8fc4af463411f13c314f2623f5a8f71dca043849
ivanvig <ivanvig@users.noreply.github.com>
2017-08-24 04:08:52 -0300

    ahora si

--- Ejercicios/LedBlink/src/Led_blink.c
+++ Ejercicios/LedBlink/src/Led_blink.c
@@ -1,13 +1,3 @@
-/*
-===============================================================================
- Name        : Led_blink.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
-===============================================================================
-*/
-
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
 #endif
@@ -15,18 +5,40 @@
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
-
+void led2Init (void); // Set GPIO - P0_22 - to be output
 // TODO: insert other definitions and declarations here
+#define LED2 22
+
+//Primera forma de hacerlo
+#define AddrFIO0DIR 0x2009C000
+#define AddrFIO0SET 0x2009C018
+#define AddrFIO0CLR 0x2009C01C
+unsigned int volatile *const FIO0DIR = (unsigned int*) AddrFIO0DIR;
+unsigned int volatile *const FIO0SET = (unsigned int*) AddrFIO0SET;
+unsigned int volatile *const FIO0CLR = (unsigned int*) AddrFIO0CLR;
 
 int main(void) {
-
     // TODO: insert code here
-
+led2Init();
     // Force the counter to be placed into memory
-    volatile static int i = 0 ;
+    int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
     while(1) {
+
+    	for(i=10000000;i>0;i--)
+    	{
+    	}
+    	*FIO0SET = (1 << LED2);	//se enciende el led
+    	for(i=10000000;i>0;i--)
+    	{
+    	}
+    	*FIO0CLR = (1 << LED2);	//se apaga el led
         i++ ;
     }
     return 0 ;
+}
+
+void led2Init (void) // Set GPIO - P0_22 - to be output
+{
+	*FIO0DIR |= (1 << LED2);
 }
