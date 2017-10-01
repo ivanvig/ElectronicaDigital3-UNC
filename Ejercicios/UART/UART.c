f257a53413b9ef31d4e66131046bfaef63c15338
SorianoJuan <ju_soriano88@hotmail.com>
2017-09-30 23:53:29 -0300

    UART andando

--- Ejercicios/UART/UART.c
+++ Ejercicios/UART/UART.c
@@ -5,17 +5,17 @@
 
 
 
-#define AddrPCONP		0x400FC0C4
-#define AddrPINSEL0		0x4002C000
-#define AddrU3LCR 		0x4000C00C
+#define AddrPCONP		0x400FC0C4	//
+#define AddrPINSEL0		0x4002C000	//
+#define AddrU3LCR 		0x4009C00C	//
 #define AddrPINMODE0 	0x4002C040
-#define AddrU3DLL 		0x4009C000
-#define AddrU3DLM 		0x4009C004
-#define AddrU3IER 		0x4009C004
-#define AddrU3RBR  		0x4009C000
-#define AddrU3THR  		0x4009C000
+#define AddrU3DLL 		0x4009C000	//
+#define AddrU3DLM 		0x4009C004	//
+#define AddrU3IER 		0x4009C004	//
+#define AddrU3RBR  		0x4009C000	//
+#define AddrU3THR  		0x4009C000	//
 #define AddrISER0  		0xE000E100
-#define AddrU3LSR 		0x4009C014
+#define AddrU3LSR 		0x4009C014	//
 
 unsigned int volatile *const PCONP = (unsigned int*) AddrPCONP;
 unsigned int volatile *const PINSEL0 = (unsigned int*) AddrPINSEL0;
@@ -35,7 +35,6 @@ void init(void);
 int main(void) {
 init();
         while(1) {
-        	*U3THR=1;
     }
     return 0 ;
 }
@@ -44,21 +43,20 @@ init();
 
 void init(void){
 	//*PCONP |= (1<<3);		//Habilitar UART0
-	*PCONP |= (1<<25);		//Habilitar UART3
-	*U3LCR |= 0x3;			//Palabra de 8 bits
+	*PCONP |= (1<<25);		//Prender UART3
+	*U3LCR |= 11;			//Palabra de 8 bits
 	*U3LCR |= (1<<7);		//Activar DLAB (Enable access to division latches para config de BR)
 	*U3DLL |= 0xA1; 		//DLL= 161 ->9585 baudios
-	*U3DLM = 0;
+	*U3DLM |= 0;			//DLM=0
 	*U3LCR &=~(1<<7);		//Habilita la interrupcion
-	*PINSEL0 |= 0xA;
-	*PINMODE0 |= 0x8;
+	*PINSEL0 |= 0xA;		//p0.0 TX y p0.1 RX
+	*PINMODE0 |= 0x8;		//Dejar al aire RX
 	*U3IER |= 0x1;			//Setea que se produzca una interrupcion
-	*ISER0 = (1<<8);
+	*ISER0 |= (1<<8);		//Habilita la interrupcion en NVIC
 }
 
 void UART3_IRQHandler(void){
-	uint8_t k;
-	k=*U3RBR;
+	char k=*U3RBR;
 	while((*U3LSR&(1<<5))==0){}		//Esta transmitiendo, entonces esperar
 	*U3THR=k;
 
